dd5e62d1225ddcfe8e24bc4a6586ba99fa011d1c
ZairaF9 <98000015+ZairaF9@users.noreply.github.com>
2022-10-26 18:01:11 -0500

    Update map-factory.js

--- src/public/js/Game/map-factory.js
+++ src/public/js/Game/map-factory.js
@@ -1,14 +1,27 @@
+import { ForestMap } from './Game/forest-map.js';
+import { DesertMap } from './Game/desert-map.js';
+import { SnowMap } from './Game/snow-map.js';
+
 export class MapFactory
 {
+
+    constructor(scene)
+    {
+        this.scene = scene;
+    }
+
     static create(name)
     {
         switch (name)
         {
             case 'Forest':
+                  return new ForestMap(this.scene);
                 break;
             case 'Snow':
+                return new DesertMap(this.scene);
                 break;
             case 'Desert':
+                return new SnowMap(this.scene);
                 break;
         }
     }
