5e8314cbc40f29d2d8a5d4d2a618ff59a12dc065
ZairaF9 <98000015+ZairaF9@users.noreply.github.com>
2022-11-22 22:12:48 -0600

    Update configuration.controller.js

--- src/app/controllers/configuration.controller.js
+++ src/app/controllers/configuration.controller.js
@@ -19,15 +19,24 @@ export class ConfigurationController {
         document.getElementsByClassName('content')[0].style.display = 'block';
         
         
-        document.getElementById('btn-graficos').addEventListener('click', function(e) {
-            document.getElementsByClassName('audio')[0].style.display = 'none';
-            document.getElementsByClassName('graficos')[0].style.display = 'block';
+       document.getElementById('btn-graficos').addEventListener('click', function(e) {
+           document.getElementsByClassName('audio')[0].style.display = 'none';
+           document.getElementsByClassName('controles')[0].style.display = 'none';
+           document.getElementsByClassName('graficos')[0].style.display = 'block';
         });
         
         document.getElementById('btn-audio').addEventListener('click', function(e) {
             document.getElementsByClassName('audio')[0].style.display = 'block';
             document.getElementsByClassName('graficos')[0].style.display = 'none';
+            document.getElementsByClassName('controles')[0].style.display = 'none';
         });
+
+        document.getElementById('btn-controles').addEventListener('click', function(e) {
+            document.getElementsByClassName('audio')[0].style.display = 'none';
+            document.getElementsByClassName('graficos')[0].style.display = 'none';
+            document.getElementsByClassName('controles')[0].style.display = 'block';
+        });
+
 
         document.getElementById('graphics-form').addEventListener('submit', function(event) {
             event.preventDefault();
