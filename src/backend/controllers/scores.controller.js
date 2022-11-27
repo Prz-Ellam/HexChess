b2d9e467a43a2554d966f50385ccea1bc617feb1
PrzEllam <PerezAlex088@outlook.com>
2022-11-27 02:59:40 -0600

    C

--- src/backend/controllers/scores.controller.js
+++ src/backend/controllers/scores.controller.js
@@ -5,9 +5,9 @@ module.exports = {
     getAll: async (req, res) => {
 
         try {
-            const scores = await User.find({}, { username: 1, victories: 1 }).sort({ 'victories': -1 });
-            console.log(scores);
-            res.json(scores);
+            //const scores = await User.find({}, { username: 1, victories: 1 }).sort({ 'victories': -1 });
+            //console.log(scores);
+            res.json({status: true});
         }
         catch (e) {
             res.json({ status: false });
