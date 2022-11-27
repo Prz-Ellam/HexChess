ab8ff52c734e44eea5f0c4f91508c8c30c04ede2
PrzEllam <PerezAlex088@outlook.com>
2022-11-27 03:04:19 -0600

    find

--- src/backend/controllers/scores.controller.js
+++ src/backend/controllers/scores.controller.js
@@ -6,6 +6,7 @@ module.exports = {
 
         try {
             //const scores = await User.find({}, { username: 1, victories: 1 }).sort({ 'victories': -1 });
+            const scores = await User.find({});
             //console.log(scores);
             res.json({status: true});
         }
