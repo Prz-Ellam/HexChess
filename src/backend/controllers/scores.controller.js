de9add4b401116dfbec8b433066fa08448d757c0
PrzEllam <PerezAlex088@outlook.com>
2022-11-27 02:54:58 -0600

    trycatch

--- src/backend/controllers/scores.controller.js
+++ src/backend/controllers/scores.controller.js
@@ -4,9 +4,14 @@ module.exports = {
 
     getAll: async (req, res) => {
 
-        const scores = await User.find({}, { username: 1, victories: 1 }).sort({ 'victories': -1 });
-        console.log(scores);
-        res.json(scores);
+        try {
+            const scores = await User.find({}, { username: 1, victories: 1 }).sort({ 'victories': -1 });
+            console.log(scores);
+            res.json(scores);
+        }
+        catch (e) {
+            res.json({ status: false });
+        }
     
     }
     
